fc9c972581b90806259a7b005cfc3af77cba38d3
Kishor Rathva <kishor.rathva@rapidops.com>
2019-05-02 12:31:52 +0530

    Almost done with set.......

--- project.js
+++ project.js
@@ -8,39 +8,37 @@ fs.readFile('./discovery.json',(err,data) => {
     // });
     console.log(projects.length);
     for(let i = 0;i< projects.length-1;i++){
+        result[projects[i]._source.project1] = new Set();
         for(let j=1;j<projects.length;j++){
-            // console.log(projects[i]._source.project1);
-            // console.log(projects[j]._source.project1);
-           
-
+        
             if(projects[i]._source.project1 === projects[j]._source.project1){
                 
         
-                    if( ! result[projects[i]._source.project1] instanceof Array);
-                        result[projects[i]._source.project1] = [];
+                    if( ! result[projects[i]._source.project1] instanceof Set)
+                        result[projects[i]._source.project1] = new Set();
                   
-                    result[projects[i]._source.project1].push(projects[i]._source.column1);
-                    result[projects[i]._source.project1].push(projects[j]._source.column1);
-
+                    result[projects[i]._source.project1].add(projects[j]._source.column1);
+                    result[projects[i]._source.project1].add(projects[i]._source.column1);
+                    
                        
             }
             if(projects[i]._source.project1 === projects[j]._source.project2){
 
-                if( ! result[projects[i]._source.project1] instanceof Array);
-                    result[projects[i]._source.project1] = [];
+                if( ! result[projects[i]._source.project1] instanceof Set)
+                    result[projects[i]._source.project1] = new  Set();
               
-                result[projects[i]._source.project1].push(projects[i]._source.column1);
-                result[projects[i]._source.project1].push(projects[j]._source.column2);
+                result[projects[i]._source.project1].add(projects[i]._source.column1);
+                result[projects[i]._source.project1].add(projects[j]._source.column2);
                 
             
             }
             if(projects[i]._source.project2 === projects[j]._source.project2){
 
-                if( ! result[projects[i]._source.project1] instanceof Array);
-                    result[projects[i]._source.project1] = [];
+                if( ! result[projects[i]._source.project1] instanceof Set)
+                    result[projects[i]._source.project1] = new Set();
               
-                result[projects[i]._source.project1].push(projects[i]._source.column2);
-                result[projects[i]._source.project1].push(projects[j]._source.column2);
+                result[projects[i]._source.project1].add(projects[i]._source.column2);
+                result[projects[i]._source.project1].add(projects[j]._source.column2);
              
             
             }
