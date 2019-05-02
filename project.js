d087911c114240bfe11ed8af4a80fa6a26f2537c
Kishor Rathva <kishor.rathva@rapidops.com>
2019-05-02 14:31:06 +0530

    Final solution till now...yet to implement to best one by reducing the for loops...

--- project.js
+++ project.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+let result = {};
 fs.readFile('./discovery.json',(err,data) => {
     if(err) throw err;
     let projects = JSON.parse(data);
-    let result = {};
+   
     // projects.forEach(element => {
     //     console.log(element._source.project1);
     // });
@@ -47,7 +48,13 @@ fs.readFile('./discovery.json',(err,data) => {
         }
         result[projects[i]._source.project1] = Array.from(result[projects[i]._source.project1]);;
     }
-    console.log(result);
+    
+    
+    console.log(`aml_investigation:${result['aml_investigation'].length}`);
+    console.log(`aml_investigation_2:${result['aml_investigation_2'].length}`);
+    console.log(`payment_flows:${result['payment_flows'].length}`);
+    console.log(`payment_transactions:${result['payment_transactions'].length}`);
+    fs.writeFileSync('output.json', JSON.stringify(result));  
 });
 
 console.log('After reading...')
