0cc15ee19fef46723f70e9a827d1c7e14bd5a327
Kishor Rathva <kishor.rathva@rapidops.com>
2019-05-02 16:07:09 +0530

    Removed if condition for instance of set

--- project.js
+++ project.js
@@ -7,7 +7,7 @@ fs.readFile('./discovery.json',(err,data) => {
     // projects.forEach(element => {
     //     console.log(element._source.project1);
     // });
-    console.log(projects.length);
+    console.log(`Main file Length:${projects.length}`);
     for(let i = 0;i< projects.length-1;i++){
         result[projects[i]._source.project1] = new Set();
         for(let j=1;j<projects.length;j++){
@@ -15,8 +15,8 @@ fs.readFile('./discovery.json',(err,data) => {
             if(projects[i]._source.project1 === projects[j]._source.project1){
                 
         
-                    if( ! result[projects[i]._source.project1] instanceof Set)
-                        result[projects[i]._source.project1] = new Set();
+                    // if( ! result[projects[i]._source.project1] instanceof Set)
+                    //     result[projects[i]._source.project1] = new Set();
                   
                     result[projects[i]._source.project1].add(projects[j]._source.column1);
                     result[projects[i]._source.project1].add(projects[i]._source.column1);
@@ -25,8 +25,8 @@ fs.readFile('./discovery.json',(err,data) => {
             }
             if(projects[i]._source.project1 === projects[j]._source.project2){
 
-                if( ! result[projects[i]._source.project1] instanceof Set)
-                    result[projects[i]._source.project1] = new  Set();
+                // if( ! result[projects[i]._source.project1] instanceof Set)
+                //     result[projects[i]._source.project1] = new  Set();
               
                 result[projects[i]._source.project1].add(projects[i]._source.column1);
                 result[projects[i]._source.project1].add(projects[j]._source.column2);
@@ -35,8 +35,8 @@ fs.readFile('./discovery.json',(err,data) => {
             }
             if(projects[i]._source.project2 === projects[j]._source.project2){
 
-                if( ! result[projects[i]._source.project1] instanceof Set)
-                    result[projects[i]._source.project1] = new Set();
+                // if( ! result[projects[i]._source.project1] instanceof Set)
+                //     result[projects[i]._source.project1] = new Set();
               
                 result[projects[i]._source.project1].add(projects[i]._source.column2);
                 result[projects[i]._source.project1].add(projects[j]._source.column2);
